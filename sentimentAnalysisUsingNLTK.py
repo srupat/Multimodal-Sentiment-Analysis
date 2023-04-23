4de590c4cf76b35c5162fdb29de7fdfca863ecd9
srupat <srujanpatwardhan@gmail.com>
2023-04-23 19:25:07 +0530

    used nltk library to perform analysis

---
+++ sentimentAnalysisUsingNLTK.py
@@ -0,0 +1,77 @@
+import string
+import nltk
+from collections import Counter
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import matplotlib.pyplot as plt
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+nltk.download('punkt') #to tokenize 
+nltk.download('stopwords') #to remove stopwords
+nltk.download('vader_lexicon')
+ 
+#most of the text on the net is of the encoding 'utf-8'
+text = open('read.txt',encoding='utf-8').read()
+
+#convert to lower case
+lower_case = text.lower()
+
+#remove all punctuations
+cleaned_text = lower_case.translate(str.maketrans('','',string.punctuation))
+
+#split the sentence into words that are then stored in a list
+tokenized_words = word_tokenize(cleaned_text,"english")
+
+#final words is a list of words that do not contain stop words
+final_words=[]
+for word in tokenized_words:
+  if word not in stopwords.words('english'):
+    final_words.append(word)
+
+emotion_list=[]
+
+#we are using with statement so that we dont have to close the file once opened
+with open('emotions.txt','r') as file:
+  for line in file:
+    
+    #replacing the new line in between 2 lines with nothing, replacing commas with nothing, replacing         apostrophe('') with nothing
+    clear_line = line.replace("\n",'').replace(",",'').replace("'",'').strip()
+
+    #word and emotion are 2 variables where the word and the emotion from repective line is stored
+    word, emotion = clear_line.split(':')
+
+    if word in final_words:
+      emotion_list.append(emotion)
+
+
+#Counter using collections library(to count emotions)
+w=Counter(emotion_list)
+
+def sentiment_analyse(sentiment_text):
+  score = SentimentIntensityAnalyzer().polarity_scores(sentiment_text)
+  neg = score['neg']
+  pos = score['pos']
+  if neg > pos:
+    print("Negative sentiment")
+  elif pos>neg:
+    print("Positive sentiment")
+  else:
+    print("Neutral sentiment")    
+
+sentiment_analyse(cleaned_text)
+
+
+#making a subplot
+fig,ax1=plt.subplots()
+
+#bar graph generation with keys(emotions) on x axis and values(count) on y axis
+ax1.bar(w.keys(),w.values())
+
+#slanting emotions below x axis
+fig.autofmt_xdate()
+
+#save to graph.png file
+plt.savefig('graph.png')
+
+#show graph on screen
+plt.show()
--- twitterAnalysis.py
+++ twitterAnalysis.py
@@ -1,13 +1,36 @@
 #referncing library with a shorter name,i.e got
-import GetOldTweets3 as got
+# import GetOldTweets3 as got
 
-def get_tweets():
-  tweetCriteria = got.manager.TweetCriteria().setQuerySearch('corona virus')\
-                                           .setSince("2019-05-01")\
-                                           .setUntil("2021-09-30")\
-                                           .setMaxTweets(10)
-  tweets = got.manager.TweetManager.getTweets(tweetCriteria)
-  text_tweets = [[tweet.text] for tweet in tweets]
-  print(text_tweets)
+# def get_tweets():
+#   tweetCriteria = got.manager.TweetCriteria().setQuerySearch('corona virus')\
+#                                            .setSince("2019-05-01")\
+#                                            .setUntil("2021-09-30")\
+#                                            .setMaxTweets(10)
+#   tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+#   text_tweets = [[tweet.text] for tweet in tweets]
+#   print(text_tweets)
 
-get_tweets()
+# get_tweets()
+import tweepy
+
+def get_tweets(query, count):
+    # Authenticate with Twitter API
+    consumer_key = 'your_consumer_key'
+    consumer_secret = 'your_consumer_secret'
+    access_token = 'your_access_token'
+    access_token_secret = 'your_access_token_secret'
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    # Fetch tweets
+    tweets = []
+    fetched_tweets = api.search(q=query, count=count)
+    for tweet in fetched_tweets:
+        tweets.append(tweet.text)
+    
+    return tweets
+
+tweets = get_tweets('Python programming', 10)
+print(tweets)
+
