f7522017420a51c07a23b0f567c66f54b715a822
srupat <srujanpatwardhan@gmail.com>
2023-06-23 08:12:26 +0530

    deleted useless files

--- main.py
+++
@@ -1,7 +0,0 @@
-print("Enter the mode of sentiment analysis that you want to perform")
-n = int(input("Enter 1 to perform Text Sentiment Analysis\nEnter 2 to perform Image Sentiment Analysis\nEnter 3 to perform Audio Sentiment Analysis\nEnter 4 to perform Video Sentiment Analysis\n"))
-
-if n==1:
-    import textSentimentAnalysis
-if n==2:
-    import imageSentimentAnalysis    
